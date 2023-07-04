b8e62e1391318cd9aae4066c25154efa9786a01e
rkshaw20 <rkshaw2026@gmail.com>
2023-07-04 17:43:26 +0530

    feat: searchBar

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -1,25 +1,106 @@
-import { Heading } from "@chakra-ui/react";
-import PostCard from "../components/PostCard";
-import { useAuthContext } from "../contexts/AuthContextProvider";
-import { useDataContext } from "../contexts/DataContextProvider";
-import { useEffect } from "react";
-import { getAllPost } from "../services/DataServices";
+import {
+  Flex,
+  Heading,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Popover,
+  PopoverArrow,
+  PopoverBody,
+  PopoverContent,
+  PopoverTrigger,
+  Text,
+  useDisclosure,
+} from '@chakra-ui/react';
+import { FiSearch } from 'react-icons/fi';
 
-const Explore=()=>{
-  const {allPost,dispatch,setLoader,}=useDataContext();
-   
+import PostCard from '../components/PostCard';
+import { useAuthContext } from '../contexts/AuthContextProvider';
+import { useDataContext } from '../contexts/DataContextProvider';
+import { useEffect, useRef, useState } from 'react';
+import { getAllPost, getAllUser } from '../services/DataServices';
+import SideBarUser from '../components/SideBarUser';
 
-  
+const Explore = () => {
+  const { allPost, allUser, dispatch, setLoader } = useDataContext();
+  const { token } = useAuthContext();
+  const [inputValue, setInputValue] = useState('');
+  const [searchSuggestion, setSearchSuggestion] = useState({});
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const initialFocusRef = useRef(null);
 
-    return (
-        <>
-          <Heading m={2} size='lg'>Explore</Heading>
-           {allPost.length>0 && allPost.map((post)=>(
-            <PostCard key={post._id} post={post} />
-           ))}
+  useEffect(() => {
+    getAllUser(token, dispatch);
+    getAllPost(token, dispatch);
+  }, []);
+
+  const handleSearch = e => {
+    const value = e.target.value.toLowerCase();
+    setInputValue(value);
+    const filteredList = allUser.filter(({ firstName, lastName, username }) => {
+      const fullName = `${firstName} ${lastName}`;
+      return (
+        fullName.toLowerCase().includes(inputValue.trim()) ||
+        username.toLowerCase().includes(inputValue.trim())
+      );
+    });
+    setSearchSuggestion(filteredList);
+  };
+  console.log(searchSuggestion);
+  return (
+    <Flex flexDir="column">
+      <Flex justifyContent="center">
+        <Popover
+          placement="bottom"
+          initialFocusRef={initialFocusRef}
+          isOpen={isOpen && inputValue.length > 0}
+        >
+          <PopoverTrigger>
+            <Flex>
+              <InputGroup>
+                <InputLeftElement>
+                  <FiSearch />
+                </InputLeftElement>
+                <Input
+                  ref={initialFocusRef}
+                  type="search"
+                  w={{ base: '', lg: '400px' }}
+                  placeholder="search user"
+                  value={inputValue}
+                  onChange={handleSearch}
+                  onBlur={onClose}
+                  onFocus={onOpen}
+                ></Input>
+              </InputGroup>
+            </Flex>
+          </PopoverTrigger>
+          <PopoverContent maxW={{ base: '12rem', lg: '22rem' }}>
+            <PopoverArrow />
+            <PopoverBody>
+            <Flex flexDir="column" gap="4" maxH="16rem" overflowY="scroll" p="2">
+
+              {searchSuggestion.length > 0 &&
+                searchSuggestion.map(userInfo => (
+                  <SideBarUser key={userInfo._id} userInfo={userInfo} isSearch/>
+                ))}
+              {searchSuggestion.length === 0 && (
+                <Text>No User with this name </Text>
+              )}
+              </Flex>
+            </PopoverBody>
            
-        </>        
-    )
-}
+          </PopoverContent>
+        </Popover>
+      </Flex>
+      {allPost.length > 0 &&
+        allPost.map(post => <PostCard key={post._id} post={post} />)}
+      <Flex />
+    </Flex>
+  );
+};
 
 export default Explore;
